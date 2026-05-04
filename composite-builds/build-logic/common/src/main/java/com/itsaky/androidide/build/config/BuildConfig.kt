efff2b9306da53dcc0e3d56916c25e440fca7e6f
Willow7737 <114362590+Willow7737@users.noreply.github.com>
2026-05-04 13:24:54 -0400

    fix: Downgrade compileSdk to 33 to resolve missing SDK 34 error

--- composite-builds/build-logic/common/src/main/java/com/itsaky/androidide/build/config/BuildConfig.kt
+++ composite-builds/build-logic/common/src/main/java/com/itsaky/androidide/build/config/BuildConfig.kt
@@ -28,7 +28,7 @@ object BuildConfig {
   const val packageName = "com.willow.androidide.ultra"
 
   /** The compile SDK version. */
-  const val compileSdk = 34
+  const val compileSdk = 33
 
   /** The minimum SDK version. */
   const val minSdk = 26
